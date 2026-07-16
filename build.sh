367465acf802d6fbc4f7efed968d690f4563245d
Ondřej Čertík <ondrej@certik.us>
2026-07-16 16:24:12 -0600

    Use --fixed-form-infer

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -ex
 
 mkdir build
 cd build
-FFLAGS="--cpp --separate-compilation --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --no-style-suggestions --no-warnings" FC=lfortran cmake ..
+FFLAGS="--cpp --separate-compilation --fixed-form-infer --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --no-style-suggestions --no-warnings" FC=lfortran cmake ..
 make
