d639135b7e2d3d0ee12959344441d29476af1286
Ondřej Čertík <ondrej@certik.us>
2026-07-17 10:14:12 -0600

    Run tests

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ mkdir build
 cd build
 FFLAGS="--cpp --separate-compilation --fixed-form-infer --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --no-style-suggestions --no-warnings" FC=lfortran cmake ..
 make
+ctest
