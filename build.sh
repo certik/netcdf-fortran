6a0c9aa04cbab5045616e40ebabe45b02257f7fd
Ondřej Čertík <ondrej@certik.us>
2026-09-10 16:37:23 -0600

    Build in parallel

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ex
 mkdir build
 cd build
 FFLAGS="--cpp --separate-compilation --fixed-form-infer --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --logical-short-circuit --no-style-suggestions --no-warnings" FC=lfortran cmake ..
-make
+make -j16
 ctest
