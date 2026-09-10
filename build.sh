e0fe477b335ea8d1f955604e9e00b05c14eb91c4
Ondřej Čertík <ondrej@certik.us>
2026-09-10 14:17:32 -0600

    build: enable --logical-short-circuit for lfortran
    nf_test guards out-of-range subscripts such as var_dimid(var_rank(i),i)
    behind .and., which the Fortran standard permits but does not require a
    processor to short-circuit. LFortran is eager by default; enable its
    --logical-short-circuit option so these guards behave as intended.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir build
 cd build
-FFLAGS="--cpp --separate-compilation --fixed-form-infer --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --no-style-suggestions --no-warnings" FC=lfortran cmake ..
+FFLAGS="--cpp --separate-compilation --fixed-form-infer --implicit-interface --implicit-typing --legacy-array-sections --use-loop-variable-after-loop --logical-short-circuit --no-style-suggestions --no-warnings" FC=lfortran cmake ..
 make
 ctest
